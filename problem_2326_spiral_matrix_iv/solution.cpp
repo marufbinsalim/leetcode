5e81f1b17d78d88dd896fa8aad41afd593f1dd86
maruf <mdmarufbinsalim@gmail.com>
2025-12-19 17:32:38 +0600

    added optimized solution to problem 2326_spiral matrix iv

--- problem_2326_spiral_matrix_iv/solution.cpp
+++ problem_2326_spiral_matrix_iv/solution.cpp
@@ -7,53 +7,37 @@ class Solution
 public:
 
     std::vector<std::vector<int>> spiralMatrix(int m, int n, ListNode* head) {
-
         int top = 0;
         int bottom = m - 1;
         int left = 0;
         int right = n - 1;
-
-        std::vector<int> ordered_list;
         std::vector<std::vector<int>> result(m, std::vector<int>(n));
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(head) {
-                    ordered_list.push_back(head->val);
-                    head = head->next;
-                }
-                else {
-                    ordered_list.push_back(-1);
-                }
-            }
-        }
-
-        int currentIndex = 0;
-        int count = 0;
         while(top <= bottom && left <= right) {
             for(int i = left; i <= right; i++) {
-                result[top][i] = ordered_list[currentIndex++];
+                result[top][i] = head ? head->val : -1;
+                head = head ? head->next : nullptr;
             }
             top++;
             for(int i = top; i <= bottom; i++) {
-                result[i][right] = ordered_list[currentIndex++];
-
+                result[i][right] = head ? head->val : -1;
+                head = head ? head->next : nullptr;
             }
             right--;
             if(top <= bottom) {
                 for(int i = right; i >= left; i--) {
-                    result[bottom][i] = ordered_list[currentIndex++];
-
+                    result[bottom][i] = head ? head->val : -1;
+                    head = head ? head->next : nullptr;
                 }
                 bottom--;
             }
             if(left <= right) {
                 for(int i = bottom; i >= top; i--){
-                    result[i][left] = ordered_list[currentIndex++];
+                    result[i][left] = head ? head->val : -1;
+                    head = head ? head->next : nullptr;
                 }
                 left++;
             }
         }
-
         return result;
     }
 };
